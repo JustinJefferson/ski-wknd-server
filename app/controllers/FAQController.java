f0d62275af50ae8d566c49826588d20cbfeb2c09
JustinJefferson <justleland@gmail.com>
2020-04-15 22:32:09 -0400

    Test deploy build

--- app/controllers/FAQController.java
+++ app/controllers/FAQController.java
@@ -2,41 +2,64 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import models.QA;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 
-import java.util.List;
+import javax.inject.Inject;
 
 public class FAQController extends Controller {
 
-    public Result save(Http.Request request){
-        JsonNode json = request.body().asJson();
-        if(json == null) return badRequest();
+    private final FormFactory formFactory;
+
+    @Inject
+    public FAQController(final FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
+    public Result create(Http.Request request) {
+        DynamicForm form = formFactory.form().bindFromRequest(request);
+        if(form == null) return badRequest();
 
         QA qa = new QA();
-        qa.setQuestion(json.findPath("question").textValue());
-        qa.setAnswer(json.findPath("answer").textValue());
+        qa.setOrderNum(form.get("order_num") == null ? null : Integer.valueOf(form.get("order_num")));
+        qa.setQuestion(form.get("question"));
+        qa.setAnswer(form.get("Answer"));
+        qa.save();
+        return ok(qa.getId().toString());
+    }
+
+    public Result read(Integer id) {
+        QA qa = QA.FIND.byId(id);
+        if(qa == null) return badRequest();
+        return ok(Json.toJson(qa));
+    }
+
+    public Result update(Http.Request request, Integer id){
+        DynamicForm form = formFactory.form().bindFromRequest(request);
+        QA qa = QA.FIND.byId(id);
+        if(form == null || qa == null) return badRequest();
+
+        qa.setOrderNum(form.get("order_num") == null ? null : Integer.valueOf(form.get("order_num")));
+        qa.setQuestion(form.get("question"));
+        qa.setAnswer(form.get("Answer"));
         qa.save();
         return ok(qa.getId().toString());
     }
 
     public Result index() {
-        JsonNode json = Json.toJson(QA.find.all());
+        JsonNode json = Json.toJson(QA.FIND.all());
         return ok(json);
     }
 
-    public Result saveIndex(Http.Request request) {
-        JsonNode json = request.body().asJson();
-        if(json == null) return badRequest();
-//        List<QA> list = Json.fromJson(json, List.class);
-
+    public Result delete(Integer id) {
+        QA qa = QA.FIND.byId(id);
+        if(qa == null) return badRequest();
+        qa.delete();
         return ok();
-    }
-
-    public Result indexAt(Http.Request request, Integer id) {
-        return ok(Json.toJson(QA.find.byId(id)));
     }
 
 
--- app/models/QA.java
+++ app/models/QA.java
@@ -4,16 +4,27 @@ import io.ebean.DB;
 import io.ebean.Ebean;
 import io.ebean.Finder;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.List;
 
 @Entity
 public class QA extends GenericModel{
 
+    @Column(name="order_num")
+    private Integer orderNum;
     private String question;
     private String answer;
     
-    public static final Finder<Integer, QA> find = new Finder<>(QA.class);
+    public static final Finder<Integer, QA> FIND = new Finder<>(QA.class);
+
+    public Integer getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(Integer orderNum) {
+        this.orderNum = orderNum;
+    }
 
     public String getQuestion() {
         return question;
